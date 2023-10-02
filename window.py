10df7e4a28494c3fa6eb0b88134fec7198fe6c06
gooodforeal <2390603@gmail.com>
2023-10-02 16:18:14 +0300

    0.0.6 (add create_button)

--- window.py
+++ window.py
@@ -10,9 +10,14 @@ class Window:
         self.root.geometry(self.window_geometry)
         self.root.resizable(resizable[0], resizable[1])
         self.lab_title = Label(self.root, text="Birthday App", font="Impact 30")
+        self.create_button = Button(text="Создать напоминание", width=100, height=2, command=self.create_task)
 
     def draw_widgets(self):
         self.lab_title.pack(anchor="n")
+        self.create_button.pack(side="bottom", pady=25)
+
+    def create_task(self):
+        pass
 
     def run(self):
         self.draw_widgets()
@@ -21,5 +26,5 @@ class Window:
 
 
 
-#wind = Window(1200, 600, "birthdayapp")
-#wind.run()
+wind = Window(1200, 600, "birthdayapp")
+wind.run()
