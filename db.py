5f473279df1cb999cbf9d054bcaa84892c0b67dc
gooodforeal <2390603@gmail.com>
2023-10-02 17:15:58 +0300

    0.1.0 (add db insert and get methods)

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 
 class Database:
@@ -11,13 +12,19 @@ class Database:
 
     def sql_create_table(self):
         curs = self.con.cursor()
-        ex = 'CREATE TABLE dates(id integer PRIMARY KEY, title text, date_of_birthday date)'
+        ex = 'CREATE TABLE dates(id integer PRIMARY KEY, title text, date_of_birthday varchar(10))'
         curs.execute(ex)
         self.con.commit()
 
     def sql_insert_into_table(self, entities):
         curs = self.con.cursor()
-        ex = 'INSERT INTO dates(id, title, date_of_birthday) VALUES(?, ?, ?)'
+        ex = 'INSERT INTO dates(title, date_of_birthday) VALUES(?, ?)'
         curs.execute(ex, entities)
         self.con.commit()
+
+    def sql_get_by_date(self, arg_date):
+        res = self.con.execute('SELECT * FROM dates WHERE date_of_birthday = ?', (arg_date,)).fetchone()
+        return res
+
+
 
